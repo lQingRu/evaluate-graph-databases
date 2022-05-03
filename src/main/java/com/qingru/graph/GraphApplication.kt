95688a7356892b73a7e48f8cce0c3c3e36ac6845
Benjie Cheong <benjie.cheong@gmail.com>
2022-05-03 18:41:18 +0800

    updated dependencies and added kotlin support

---
+++ src/main/java/com/qingru/graph/GraphApplication.kt
@@ -0,0 +1,12 @@
+package com.qingru.graph;
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GraphApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(GraphApplication::class.java, *args)
+}
+
